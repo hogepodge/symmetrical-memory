40564760f489b09c498891783586205819643958
Chris Hoge <chris@openstack.org>
2017-03-01 16:35:25 -0800

    Need epel before everything else

--- ironic/02_setup.sh
+++ ironic/02_setup.sh
@@ -1,5 +1,6 @@
 # begin by installing system dependencies
-sudo yum install -y git vim epel-release python-pip python-devel \
+sudo yum install -y epel-release
+sugo yum install -y git vim python-pip python-devel \
   libffi-devel gcc openssl-devel ansible ntp python-virtualenv \
   pyton-netaddr screen
 
