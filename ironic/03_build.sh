35152f6985f15f2b1ed3ba9d49ced336c36b41b7
Chris Hoge <chris@openstack.org>
2017-03-02 10:18:28 -0800

    Using the hardware again.
    I really should refactor this to be ip independent

--- ironic/03_build.sh
+++ ironic/03_build.sh
@@ -1,4 +1,4 @@
 # as root
 cd ~/hoge/kolla
 source .kolla/bin/activate
-kolla-build --registry 192.168.11.131:4000 --push -t source -T 8
+kolla-build --registry 192.168.1.23:4000 --push -t source -T 8
