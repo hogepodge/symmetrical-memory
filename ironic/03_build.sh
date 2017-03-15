1eca592bfb4efab453abcac9e0cc6185956dbbed
Chris Hoge <chris@hogepodge.com>
2017-03-15 14:02:50 -0700

    Another operational update

--- ironic/03_build.sh
+++ ironic/03_build.sh
@@ -1,4 +1,4 @@
 # as root
 cd /home/hoge/kolla
 source .kolla/bin/activate
-kolla-build --registry 192.168.1.23:4000 --push -t source -T 8
+kolla-build --registry 192.168.1.6:4000 --push -t source -T 8
