57d137d98e92c522e244a42a09bc6d3cbfbcafdd
Chris Hoge <chris@openstack.org>
2017-03-17 13:14:44 -0700

    Temp working files

--- docker/30_slab_imagevolume.sh
+++ docker/30_slab_imagevolume.sh
@@ -1,1 +1,8 @@
+# Start by dropping in the local files that everyone will want
+sudo mkdir /imagevolume
+sudo mkdir /imagevolume/tftpboot
+sudo mkdir /imagevolume/tftpboot/pxelinux.cfg
+sudo mkdir /imagevolume/httpboot
+sudo mkdir /imagevolume/httpboot/pxelinux.cfg
+sudo cp tftp/map-file /imagevolume/tftpboot/map-file
 docker run -v /imagevolume/tftpboot:/tftpboot:z -v /imagevolume/httpboot:/httpboot:z --name slab_imagevolume -id alpine sh
---
+++ docker/40_slab_tftp.sh
@@ -0,0 +1,1 @@
+docker run --name slab_tftp -d -v slab_tftbootvolume: slab_tftp
